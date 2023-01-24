2326a0c7fe44dbe89609b37a1b53addc2ca6853d
Yuriy <kim.u.907@gmail.com>
2023-01-24 19:52:34 +0600

    release3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const getClimber = document.querySelector("#climber");
+const getFlag = document.querySelectorAll(".flag");
+let climberPosition = 0;
 //Создание альпиниста
-
-function moveClimberUp() {
-  if (climberPosition < 4) {
+function forward() {
+  if (climberPosition < 9) {
     document.querySelector("#climber").remove();
 
     const climber = document.createElement("img");
@@ -11,12 +13,16 @@ function moveClimberUp() {
     climber.src = "./img/climber.png";
     const getCheckPoints = document.getElementsByClassName("checkpoint");
 
+    if (climberPosition == 4 || climberPosition == 5) {
+      //   const getClimber = document.querySelector("#climber");
+      climber.style.transform = "rotate3d(0, 1, 1, -75deg)";
+    }
     getCheckPoints[Number(climberPosition + 1)].append(climber);
     climberPosition++;
   }
 }
 
-function moveClimberDown() {
+function backward() {
   if (climberPosition > 0) {
     document.querySelector("#climber").remove();
 
@@ -39,21 +45,38 @@ function readyForSlalom() {
 }
 
 function getFlagFunc() {
-  const getFlag = document.querySelector(".flag");
-  getFlag.style.visibility = "visible";
+  if (climberPosition === 4) {
+    getFlag[0].style.visibility = "visible";
+  }
+  if (climberPosition === 9) {
+    getFlag[1].style.visibility = "visible";
+  }
 }
 
-let climberPosition = 0;
+function getMessage1() {
+  const getText1 = document.querySelector(".text1");
+  getText1.style.visibility = "visible";
+}
+
+function getMessage2() {
+  const getText2 = document.querySelector(".text2");
+  getText2.style.visibility = "visible";
+}
 
 document.addEventListener("keydown", (event) => {
   if (event.code == "KeyX") {
-    moveClimberUp();
-    if (climberPosition == 4) {
-      //   window.getComputedStyle(document.querySelector(".five"), ":before").getPropertyValue('visibility');
-      setTimeout(getFlagFunc, 3000);
-      setTimeout(readyForSlalom, 5000);
+    forward();
+    if (climberPosition == 4 || climberPosition == 9) {
+      setTimeout(getFlagFunc, 1000);
+      if (climberPosition == 4) {
+        getMessage1();
+        setTimeout(readyForSlalom, 500);
+      }
+    }
+    if (climberPosition == 9) {
+      getMessage2();
     }
   } else if (event.code == "KeyZ") {
-    moveClimberDown();
+    backward();
   }
 });
