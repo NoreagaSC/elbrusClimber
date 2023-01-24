797aea3431cf66d44d8b213aafbb03c7154184b0
Yuriy <kim.u.907@gmail.com>
2023-01-24 18:09:10 +0600

    release2.1

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 //Создание альпиниста
-function moveClimber() {
+
+function moveClimberUp() {
   if (climberPosition < 4) {
     document.querySelector("#climber").remove();
-  }
-  const climber = document.createElement("img");
-  climber.id = "climber";
-  climber.style.width = "40px";
-  climber.style.height = "70px";
-  climber.src = "./img/climber.png";
 
-  const getCheckPoints = document.getElementsByClassName("checkpoint");
-  getCheckPoints[Number(climberPosition + 1)].append(climber);
-  if (climberPosition < 6) {
+    const climber = document.createElement("img");
+    climber.id = "climber";
+    climber.style.width = "40px";
+    climber.style.height = "70px";
+    climber.src = "./img/climber.png";
+    const getCheckPoints = document.getElementsByClassName("checkpoint");
+
+    getCheckPoints[Number(climberPosition + 1)].append(climber);
     climberPosition++;
+  }
+}
+
+function moveClimberDown() {
+  if (climberPosition > 0) {
+    document.querySelector("#climber").remove();
+
+    const climber = document.createElement("img");
+    climber.id = "climber";
+    climber.style.width = "40px";
+    climber.style.height = "70px";
+    climber.src = "./img/climber.png";
+    const getCheckPoints = document.getElementsByClassName("checkpoint");
+
+    getCheckPoints[Number(climberPosition - 1)].append(climber);
+    climberPosition--;
   }
 }
 
@@ -25,12 +41,12 @@ let climberPosition = 0;
 
 document.addEventListener("keydown", (event) => {
   if (event.code == "KeyX") {
-    moveClimber();
-    if (climberPosition >= 4) {
+    moveClimberUp();
+    if (climberPosition == 4) {
       //   window.getComputedStyle(document.querySelector(".five"), ":before").getPropertyValue('visibility');
       setTimeout(getFlagFunc, 3000);
     }
   } else if (event.code == "KeyZ") {
-    console.log("z key is pressed");
+    moveClimberDown();
   }
 });
