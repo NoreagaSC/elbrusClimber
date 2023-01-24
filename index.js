7c102f0c7b679b3dd593b2d6f08b3eee4560877e
Yuriy <kim.u.907@gmail.com>
2023-01-24 14:56:38 +0600

    release1.1 climber can move up

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 //Создание альпиниста
-const climber = document.createElement("img");
-climber.id = "climb";
-climber.style.width = "40px";
-climber.style.height = "70px";
-climber.src = "./img/climber.png";
+function moveClimber() {
+  document.querySelector("#climber").remove();
+  const climber = document.createElement("img");
+  climber.id = "climber";
+  climber.style.width = "40px";
+  climber.style.height = "70px";
+  climber.src = "./img/climber.png";
 
-const getCheckPoints = document.getElementsByClassName("checkpoint");
-getCheckPoints[1].append(climber);
+  const getCheckPoints = document.getElementsByClassName("checkpoint");
+  getCheckPoints[Number(climberPosition + 1)].append(climber);
+  climberPosition++;
+}
+
+let climberPosition = 0;
 
 document.addEventListener("keydown", (event) => {
   if (event.code == "KeyX") {
     console.log("x key is pressed");
+    moveClimber();
   } else if (event.code == "KeyZ") {
     console.log("z key is pressed");
   }
