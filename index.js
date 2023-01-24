88dd1a9e0747a7f3b209865dd79d081e40c0598b
Yuriy <kim.u.907@gmail.com>
2023-01-24 16:20:53 +0600

    release2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Создание альпиниста
 function moveClimber() {
-  document.querySelector("#climber").remove();
+  if (climberPosition < 4) {
+    document.querySelector("#climber").remove();
+  }
   const climber = document.createElement("img");
   climber.id = "climber";
   climber.style.width = "40px";
@@ -9,15 +11,25 @@ function moveClimber() {
 
   const getCheckPoints = document.getElementsByClassName("checkpoint");
   getCheckPoints[Number(climberPosition + 1)].append(climber);
-  climberPosition++;
+  if (climberPosition < 6) {
+    climberPosition++;
+  }
+}
+
+function getFlagFunc() {
+  const getFlag = document.querySelector(".flag");
+  getFlag.style.visibility = "visible";
 }
 
 let climberPosition = 0;
 
 document.addEventListener("keydown", (event) => {
   if (event.code == "KeyX") {
-    console.log("x key is pressed");
     moveClimber();
+    if (climberPosition >= 4) {
+      //   window.getComputedStyle(document.querySelector(".five"), ":before").getPropertyValue('visibility');
+      setTimeout(getFlagFunc, 3000);
+    }
   } else if (event.code == "KeyZ") {
     console.log("z key is pressed");
   }
